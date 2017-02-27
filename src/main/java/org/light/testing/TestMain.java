0efae168cde50c204d262466a34ca68c5a6f4e6f
xiaoming.wang <18600882246@163.com>
2017-02-27 20:53:00 +0800

    add test class

---
+++ src/main/java/org/light/testing/TestMain.java
@@ -0,0 +1,31 @@
+package org.light.testing;
+
+/**
+ * @author Michael.Wang
+ * @date 2017/2/27
+ */
+public class TestMain {
+
+    public static void main(String[] args) throws InterruptedException {
+        test3();
+        while (true) {
+            test();
+        }
+    }
+
+    public static void test() throws InterruptedException {
+        Thread.sleep(1000);
+        System.out.println("test");
+        test2();
+    }
+
+    public static void test2() throws InterruptedException {
+        Thread.sleep(2000);
+        System.out.println("test2");
+    }
+
+    public static void test3() throws InterruptedException {
+        Thread.sleep(1000);
+        System.out.println("test3");
+    }
+}
