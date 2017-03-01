018e187abb395754482775c989743c8f576e333f
xiaoming.wang <18600882246@163.com>
2017-03-01 11:38:43 +0800

    add mock ---temp version

--- src/test/java/com/taobao/profile/thread/TimeControlThreadTest.java
+++ src/test/java/com/taobao/profile/thread/TimeControlThreadTest.java
@@ -1,18 +1,36 @@
 package com.taobao.profile.thread;
 
+import com.taobao.profile.Manager;
 import com.taobao.profile.config.ProfConfig;
+import mockit.NonStrictExpectations;
 import org.junit.Test;
 
+import java.util.Calendar;
+
 public class TimeControlThreadTest {
+    public static void main(String[] args) throws Exception {
+        new TimeControlThreadTest(). testRun();
+    }
 
     @Test
     public void testRun() throws Exception {
+        final Calendar cal = Calendar.getInstance();
+        cal.set(2017, Calendar.MARCH, 1, 10, 55, 03); //2017/3/1 10:55:03
+
+        new NonStrictExpectations(){{
+            System.currentTimeMillis(); //指定要mock的方法
+            result =  cal.getTime().getTime(); //指定mock方法要返回的结果
+            times=0;
+        }};
+
+        Manager.instance().initialization();
         ProfConfig config = new ProfConfig();
         config.setStartProfTime("09:00:00");
-        config.setStartProfTime("23:00:00");
+        config.setEndProfTime("11:27:07");
         config.setDebugMode(true);
-        Thread t = new TimeControlThread(config);
-        t.start();
+
+        TimeControlThread t = new TimeControlThread(config);
+        t.run();
     }
 
     @Test
