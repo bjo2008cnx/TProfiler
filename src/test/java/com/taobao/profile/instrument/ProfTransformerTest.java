17511ff8295a217ec8da6e3ea6be2364d05b6d0b
xiaoming.wang <18600882246@163.com>
2017-03-03 14:45:29 +0800

    refactor

--- src/test/java/com/taobao/profile/instrument/ProfTransformerTest.java
+++ src/test/java/com/taobao/profile/instrument/ProfTransformerTest.java
@@ -1,5 +1,7 @@
 package com.taobao.profile.instrument;
 
+import com.taobao.profile.Manager;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.FileInputStream;
@@ -8,12 +10,21 @@ public class ProfTransformerTest {
 
     @Test
     public void testTransform() throws Exception {
-
+        Manager.instance().initialization();
         byte[] bytes = StreamUtil.stream2Byte(new FileInputStream("d:/Comparable.class"));
         System.out.println(bytes.length);
+        for (byte b : bytes) {
+            System.out.print(b);
+        }
         byte[] transformed = new ProfTransformer().transform(null, "com.asm3.Comparable", null, null, bytes);
-        System.out.println(transformed.length);
         System.out.println();
-
+        for (byte b : transformed) {
+            System.out.print(b);
+        }
+        try {
+            Assert.assertArrayEquals(bytes, transformed);
+            Assert.fail("should not be equal.");
+        }catch (Exception e){
+        }
     }
 }
--- src/test/java/common/TestMain.java
+++ src/test/java/common/TestMain.java
@@ -7,26 +7,58 @@ package common;
 public class TestMain {
 
     public static void main(String[] args) throws InterruptedException {
-        TestMain testMain = new TestMain();
-        testMain.test3();
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                final TestMain testMain = new TestMain();
+                try {
+                    testMain.test();
+                    testMain.test4();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        t.start();
+
         while (true) {
-            testMain.test();
+            StackTraceElement[] stacks = t.getStackTrace();
+            int i = 0;
+            for (StackTraceElement stack : stacks) {
+                String output = String.format("Class:%s:%s:%s", stack.getClassName(), stack.getMethodName(), stack.getLineNumber());
+                for (int k = 0; k < i; k++) {
+                    System.out.print("  ");
+                }
+                i++;
+                System.out.println(output);
+            }
+            Thread.sleep(100);
         }
     }
 
     public void test() throws InterruptedException {
+        test2();
         Thread.sleep(1000);
-        System.out.println("test");
-        new TestMain().test2();
+        log("test");
     }
 
     public void test2() throws InterruptedException {
+        test3();
         Thread.sleep(2000);
-        System.out.println("test2");
+        log("test2");
     }
 
     public void test3() throws InterruptedException {
         Thread.sleep(1000);
-        System.out.println("test3");
+        log("test3");
+    }
+
+    public void test4() throws InterruptedException {
+        Thread.sleep(1000);
+        log("test4");
+    }
+
+    private void log(String s) {
+        // System.out.println(s);
     }
 }
