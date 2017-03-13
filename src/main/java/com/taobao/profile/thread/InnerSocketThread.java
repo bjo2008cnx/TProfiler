2165ac6120762bd2ab57633c989e02acdcbd423d
xiaoming.wang <18600882246@163.com>
2017-03-13 21:11:33 +0800

    add testId

--- src/main/java/com/taobao/profile/thread/InnerSocketThread.java
+++ src/main/java/com/taobao/profile/thread/InnerSocketThread.java
@@ -4,117 +4,91 @@
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * version 2 as published by the Free Software Foundation.
- * 
+ *
  */
 package com.taobao.profile.thread;
 
 import com.taobao.profile.Manager;
 import com.taobao.profile.ProfilerConstant;
 import com.taobao.profile.runtime.MethodCache;
+import lombok.extern.slf4j.Slf4j;
+import org.lightfw.util.io.common.StreamUtil;
 
-import java.io.*;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.SocketException;
 
 /**
  * 对外提供Socket开关
- * 
- * @author shutong.dy
- * @since 2012-1-11
  */
+@Slf4j
 public class InnerSocketThread extends Thread {
-	/**
-	 * server
-	 */
-	private ServerSocket socket;
+    /**
+     * server
+     */
+    private ServerSocket socket;
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see java.lang.Thread#run()
-	 */
-	public void run() {
-		try {
-			socket = new ServerSocket(Manager.PORT);
-			while (true) {
-				Socket child = socket.accept();
+    public void run() {
+        try {
+            socket = new ServerSocket(Manager.PORT);
+            while (true) {
+                handleCommand();
+            }
+        } catch (IOException e) {
+            log.error("io error", e);
+        } finally {
+            StreamUtil.close(socket);
+        }
+    }
 
-				child.setSoTimeout(5000);
+    /**
+     * 处理客户端发送过来的命令
+     *
+     * @throws IOException
+     */
+    private void handleCommand() throws IOException {
+        Socket childSocket = socket.accept();
+        childSocket.setSoTimeout(5000);
+        String commands = StreamUtil.streamToString(childSocket.getInputStream());
+        String[] commandStrs = commands.split(":");
+        String command = commandStrs[0];
+        if (ProfilerConstant.START.equals(command)) {
+            Manager.instance().setSwitchFlag(true);
+        } else if (ProfilerConstant.STATUS.equals(command)) {
+            write(childSocket.getOutputStream());
+        } else if (ProfilerConstant.FLUSHMETHOD.equals(command)) {
+            MethodCache.flushMethodData();
+        } else {
+            Manager.instance().setSwitchFlag(false);
+        }
+        StreamUtil.close(childSocket);
+    }
 
-				String command = read(child.getInputStream());
-
-				if (ProfilerConstant.START.equals(command)) {
-					Manager.instance().setSwitchFlag(true);
-				} else if (ProfilerConstant.STATUS.equals(command)) {
-					write(child.getOutputStream());
-				} else if (ProfilerConstant.FLUSHMETHOD.equals(command)) {
-					MethodCache.flushMethodData();
-				} else {
-					Manager.instance().setSwitchFlag(false);
-				}
-				child.close();
-			}
-		} catch (SocketException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			if (socket != null) {
-				try {
-					socket.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-	}
-
-	/**
-	 * 读取输入流
-	 * 
-	 * @param in
-	 * @return
-	 * @throws IOException
-	 */
-	private String read(InputStream in) throws IOException {
-		BufferedInputStream bin = new BufferedInputStream(in);
-		StringBuffer sb = new StringBuffer();
-        int i;
-		while ((i = bin.read()) != -1) {
-			char c = (char) i;
-			if (c == '\r') {
-				break;
-			} else {
-				sb.append(c);
-			}
-		}
-		return sb.toString();
-	}
-
-	/**
-	 * 输出状态
-	 * 
-	 * @param os
-	 * @throws IOException
-	 */
-	private void write(OutputStream os) throws IOException {
-		BufferedOutputStream out = new BufferedOutputStream(os);
-		if (Manager.instance().getSwitchFlag()) {
-			out.write("running".getBytes());
-		} else {
-			out.write("stop".getBytes());
-		}
-		out.write('\r');
-		out.flush();
-	}
+    /**
+     * 输出状态
+     *
+     * @param os
+     * @throws IOException
+     */
+    private void write(OutputStream os) throws IOException {
+        BufferedOutputStream out = new BufferedOutputStream(os);
+        if (Manager.instance().getSwitchFlag()) {
+            out.write("running".getBytes());
+        } else {
+            out.write("stop".getBytes());
+        }
+        out.write('\r');
+        out.flush();
+    }
 
     /**
      * 调试使用
      *
      * @param args
      */
-    public static void main(String[] args){
+    public static void main(String[] args) {
         InnerSocketThread socketThread = new InnerSocketThread();
         socketThread.setName("TProfiler-InnerSocket-Debug");
         socketThread.start();
