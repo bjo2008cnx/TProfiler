d1418157e8c32087f1bfcb788cfb253c855de941
xiaoming.wang <18600882246@163.com>
2017-03-15 15:47:07 +0800

    add testcase

---
+++ src/test/java/com/taobao/profile/analysis/ProfilerLogAnalysisTest.java
@@ -0,0 +1,16 @@
+package com.taobao.profile.analysis;
+
+import org.junit.Test;
+
+public class ProfilerLogAnalysisTest {
+
+    @Test
+    public void testGetTimeSortData() throws Exception {
+
+    }
+
+    @Test
+    public void testPrintResult() throws Exception {
+
+    }
+}
---
+++ src/test/java/com/taobao/profile/client/TProfilerClientTest.java
@@ -0,0 +1,26 @@
+package com.taobao.profile.client;
+
+import org.junit.Test;
+
+public class TProfilerClientTest {
+
+    @Test
+    public void testStart() throws Exception {
+        TProfilerClient.start("localhost",30000);
+    }
+
+    @Test
+    public void testStop() throws Exception {
+
+    }
+
+    @Test
+    public void testStatus() throws Exception {
+
+    }
+
+    @Test
+    public void testFlushMethod() throws Exception {
+
+    }
+}
